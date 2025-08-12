f101ce224c0f03d6617b48bbd30ea44596c779a5
Yahwanth Sabbithi <yahwanth.sabbithi@ispace.com>
2025-08-12 06:31:01 +0530

    refact(Twilio):Added new way of verification using Static API

--- apps/backend/src/twilio/twilio.service.ts
+++ apps/backend/src/twilio/twilio.service.ts
@@ -65,7 +65,7 @@ export class TwilioService {
         {
           headers: {
             ...form.getHeaders(),
-            Authorization: apiToken,
+            Authorization: `Bearer ${apiToken}`,
           },
         },
       );
