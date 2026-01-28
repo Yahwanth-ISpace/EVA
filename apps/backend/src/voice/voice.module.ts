34068d4d4e67a7da5c57892ed7e6e5d684396634
Yahwanth Sabbithi <yahwanth.sabbithi@ispace.com>
2026-01-29 00:53:47 +0530

    fix(Voice):Added service to serve the voice file created by eleven

--- apps/backend/src/voice/voice.module.ts
+++ apps/backend/src/voice/voice.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ElevenLabsService } from './elevenlabs.service';
 import { ElevenLabsAudioStackService } from './elevenlabs-audio-stack.service';
+import { AudioController } from './audio.controller';
 
 @Module({
+  controllers: [AudioController],
   providers: [ElevenLabsService, ElevenLabsAudioStackService],
   exports: [ElevenLabsService, ElevenLabsAudioStackService],
 })
