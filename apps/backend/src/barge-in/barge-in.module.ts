5a0806c3c2e8a7045b35bc1e52c040a2e50622b9
Yahwanth Sabbithi <yahwanth.sabbithi@ispace.com>
2026-09-21 17:43:08 +0530

    fix(backend): Barge-in Dependecies edit

--- apps/backend/src/barge-in/barge-in.module.ts
+++ apps/backend/src/barge-in/barge-in.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { BargeInController } from './barge-in.controller';
 import { BargeInService } from './barge-in.service';
-import { MediaStreamHandlerService } from 'src/twilio/media-stream.handler';
+
+import { TwilioModule } from 'src/twilio/twilio.module';
+import { TranscriptionModule } from 'src/transcription/transcription.module';
 
 @Module({
+  imports: [
+    TranscriptionModule,
+    TwilioModule,
+  ],
   controllers: [BargeInController],
-
-  providers: [BargeInService, MediaStreamHandlerService],
-
+  providers: [BargeInService],
   exports: [BargeInService],
 })
 export class BargeInModule {}
