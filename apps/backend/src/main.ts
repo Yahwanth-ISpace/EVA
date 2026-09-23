8185aefcdafaa944c7ab4bd5e9abf761bb575e9e
Yahwanth Sabbithi <yahwanth.sabbithi@ispace.com>
2026-09-23 14:55:08 +0530

    fix(backend):reverting the changes.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
   app.enableCors({
-    origin: ['http://3.210.91.177', 'http://localhost:5173'],
+    origin: corsOrigin(),
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
       'Content-Type',
